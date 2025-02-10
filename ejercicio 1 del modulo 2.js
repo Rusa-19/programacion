67e12503ca8b72479556ea60c5f160b79a964f46
Janna <jannaextra01@gmail.com>
2025-02-10 14:30:15 -0400

    Add files via upload

---
+++ ejercicio 1 del modulo 2.js
@@ -0,0 +1,14 @@
+let suma = 7 + 3;
+console.log ("El resultado:" + suma)
+
+let resta = 10 - 4 ;
+console.log ("El resultado:" + resta)
+
+let multiplicacion = 6 * 2 ;
+console.log ("El resultado:" + multiplicacion)
+
+let dividido = 15 / 3 ;
+console.log ("El resultado:" + dividido)
+
+let division = 9 % 4 ;
+console.log ("El resultado:" + division)
